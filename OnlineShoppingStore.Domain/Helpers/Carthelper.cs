56ac5fcd52fec75b4e044d5529a7e087956f7878
Your Name <you@example.com>
2018-02-08 10:54:26 +0530

    corrected the issuse and added the changes

---
+++ OnlineShoppingStore.Domain/Helpers/Carthelper.cs
@@ -0,0 +1,22 @@
+﻿using OnlineShoppingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Helpers
+{
+    public  class Carthelper
+    {
+        public Cart GetCart(Cart SessionOfCart)
+        {
+            Cart cart = SessionOfCart;
+            if(cart == null)
+            {
+                cart = new Cart();               
+            }
+            return cart;
+        }
+    }
+}
---
+++ OnlineShoppingStore.WEBUI/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingStore.Domain.Helpers;
+using OnlineShoppingStore.WEBUI.Models;
+
+namespace OnlineShoppingStore.WEBUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        private Carthelper CartHelper;
+        
+        public CartController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public RedirectToRouteResult AddToCart(int ProductId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == ProductId);
+            if(product != null)
+            {
+                //Cart CartSession = (Cart)Session["Cart"];
+                //CartHelper.GetCart(CartSession).AddItem(product, 1);
+                GetCart().AddItem(product,1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveProduct(int ProductId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(f => f.ProductId == ProductId);
+            if(product != null)
+            {
+                Cart cart = (Cart)Session["Cart"];
+                CartHelper.GetCart(cart).RemoveItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        
+        // GET: Cart
+        public ViewResult Index(string ReturnUrl)
+        {
+            //Cart cart = CartHelper.GetCart((Cart)Session["Cart"]);
+            Cart cart = GetCart();
+            CartIndexViewModel indexViewModel = new CartIndexViewModel();
+            indexViewModel.cart = cart;
+            indexViewModel.ReturnUlr = ReturnUrl;
+
+            return View(indexViewModel);
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
